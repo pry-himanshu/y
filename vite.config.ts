bbae1f9d75537e0d2586e7b178350fc5ec963408
pry-himanshu <pry.himanshu@gmail.com>
2026-03-12 23:30:35 +0530

    Fix Vite base path for Vercel and GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+const basePath = process.env.VITE_BASE_PATH || '/';
+
 export default defineConfig({
-  base: '/y/',
+  base: basePath,
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
